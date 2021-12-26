2c4dd1d6ed6b643ef243faaa2c3f3797fa093624
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2021-12-27 02:40:24 +0530

    Update 1. Reverse digits.cpp

--- code/GFG/Google Test/01. Mathematics/1. Reverse digits.cpp
+++ code/GFG/Google Test/01. Mathematics/1. Reverse digits.cpp
@@ -1,16 +1,16 @@
 class Solution
 {
 	public:
-		long long int reverse_digit(long long int n)
+	long long int reverse_digit(long long int n)
+	{
+		// Code here
+		long result=0, remainder=0;
+		while(n>0)
 		{
-		    // Code here
-		    long result=0, remainder=0;
-            while(n>0)
-            {
-                remainder = n%10;
-                result = result*10 + remainder;
-                n = n/10;   
-            }
-            return result;
+			remainder = n%10;
+			result = result*10 + remainder;
+			n = n/10;   
 		}
+		return result;
+	}
 }
